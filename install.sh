807182d5721cd3f5bed814d4a46f25f1def4f72f
Erich L Foster <erichlf@gmail.com>
2024-02-06 14:08:58 +0100

    Add stow

--- install.sh
+++ install.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
-DOTFILES=$HOME/dotfiles
+DOTFILES_DIR=$HOME/dotfiles
 
-cd $DOTFILES
+cd $DOTFILES_DIR
 
 git submodule init
 git submodule update
 
-cd $HOME
-
-# spacevim setup
-ln -sf $DOTFILES/SpaceVim $HOME/.vim
-ln -sf $DOTFILES/.SpaceVim.d $HOME/
-
-# setup git
-ln -sf $DOTFILES/.gitconfig $HOME/
-ln -sf $DOTFILES/.gitexcludes $HOME/
-
-# oh-my-bash & plugins
-cat $DOTFILES/.bashrc >> $HOME/.bashrc
-ln -sf $DOTFILES/.aliases $HOME/
-ln -sf $DOTFILES/.oh-my-bash $HOME/
+sudo apt-get update
+sudo apt-get install stow wget unzip -y
+stow -v --adopt --dir $DOTFILES_DIR --target $HOME --stow my-home
+# if the adopt made a local change then undo that
+git checkout HEAD -- starship my-home
 
 # setup ble.sh
 sudo apt-get update
 sudo apt-get install -y gawk
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=$HOME/.local
+rm -rf ble.sh
 
 # setup starship
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
 unzip DroidSansMono.zip -d $HOME/.fonts
 fc-cache -fv
+rm -f DroidSansMono.zip
 
 curl -sS https://starship.rs/install.sh -o starship.sh 
 chmod +x starship.sh
 sudo ./starship.sh -y
 mkdir -p $HOME/.config
-ln -sf $DOTFILES/starship.toml $HOME/.config/
+stow -v --adopt --dir $DOTFILES_DIR --target $HOME/.config/ --stow starship
 rm -f starship.sh
