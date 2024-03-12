07f7aac7c39052c46719d90cb1ebd436e20cd58a
Erich L Foster <erichlf@gmail.com>
2024-03-12 09:52:28 +0000

    Switch to zsh

--- install.sh
+++ install.sh
@@ -9,17 +9,11 @@ git submodule update
 
 sudo apt-get update
 sudo apt-get install stow wget unzip -y
+mkdir -p $HOME/.config
 stow -v --adopt --dir $DOTFILES_DIR --target $HOME --stow my-home
-cat $DOTFILES_DIR/.bashrc >> $HOME/.bashrc
-# if the adopt made a local change then undo that
+stow -v --adopt --dir $DOTFILES_DIR --target $HOME/.config/ --stow starship
+# if the stow adopt made a local change then undo that
 git checkout HEAD -- starship my-home
-
-# setup ble.sh
-sudo apt-get update
-sudo apt-get install -y gawk
-git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
-make -C ble.sh install PREFIX=$HOME/.local
-rm -rf ble.sh
 
 # setup starship
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip
@@ -27,9 +21,7 @@ unzip DroidSansMono.zip -d $HOME/.fonts
 fc-cache -fv
 rm -f DroidSansMono.zip
 
-curl -sS https://starship.rs/install.sh -o starship.sh 
+curl -sS https://starship.rs/install.sh -o starship.sh
 chmod +x starship.sh
 sudo ./starship.sh -y
-mkdir -p $HOME/.config
-stow -v --adopt --dir $DOTFILES_DIR --target $HOME/.config/ --stow starship
 rm -f starship.sh
